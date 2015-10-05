5c43b2b5bf0be0b02be7fb9a3109be4605f14202
briapete <brian.peterson@kineticdata.com>
2015-10-05 13:57:14 -0500

    Initial Commit
    v1

---
+++ test/sample_input.rb
@@ -0,0 +1,22 @@
+{
+  'info' => {
+    'server'               => '',
+    'username'             => '',
+    'password'             => '',
+    'port'                 => '0',
+    'prognum'              => '0',
+    'authentication'       => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'task_name'          => "",
+    'summary'            => "",
+    'notes'              => "",
+    'priority'           => "",
+    'sequence_number'    => "",
+    'location_company'   => "",
+    'parent_form_name'   => "",
+    'parent_id'          => "",
+    'parent_instance_id' => ""
+  }
+}
